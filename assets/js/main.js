37fa286c57b64be374d7789712c1604850cd8fa7
jasonchn18 <jasonchn18@gmail.com>
2023-02-05 11:11:59 +0800

    Add Scroll Up feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -196,14 +196,14 @@ function scrollHeader() {
 window.addEventListener('scroll', scrollHeader)
 
 /*==================== SHOW SCROLL UP ====================*/ 
-// function scrollTop() {
-//     const scrollTop = document.getElementById('scroll-top');
-//     // When the scroll is higher than 560 viewport height, 
-//         // add the show-scroll class to the a tag with the scroll-top id
-//     if (this.scrollY >= 560) { scrollTop.classList.add('show-scroll'); }
-//     else { scrollTop.classList.remove('show-scroll') }
-// }
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, 
+        // add the show-scroll class to the a tag with the scroll-top id
+    if (this.scrollY >= 560) { scrollUp.classList.add('show-scroll'); }
+    else { scrollUp.classList.remove('show-scroll') }
+}
 
-// window.addEventListener('scroll', scrollTop)
+window.addEventListener('scroll', scrollUp)
 
 /*==================== DARK LIGHT THEME ====================*/ 
